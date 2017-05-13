f5914f2759e20eb3e86d18948001f0af3d475c3a
JaVillarroell <javivillarroell@gmail.com>
2017-05-13 15:37:18 -0400

    Add files via upload

--- PAgina web 1/js/descripcion.js
+++ PAgina web 1/js/descripcion.js
@@ -1,40 +1,39 @@
-/**
- * Created by Usuario on 11/5/2017.
- */
+
 var nBusq ;
 cargadorDescripcion();
-function anadirNumBusqueda(){
-    
-    prodMostrar = JSON.parse(localStorage.getItem("selectedItem"));
-    nBusq = parseInt(prodMostrar.busquedas)+1;
-    console.log(nBusq);
-    buscarMas();
-}
 function cargadorDescripcion()
 {
     prodMostrar = JSON.parse(localStorage.getItem("selectedItem"));
+    prodImagen=JSON.parse(localStorage.getItem("selectedimagen"));
     console.log( prodMostrar.name);
     document.getElementById("nombreProd").innerHTML = prodMostrar.name;
     document.getElementById("precio").innerHTML = prodMostrar.precio;
     document.getElementById("cantidad").innerHTML = prodMostrar.cantidad;
     document.getElementById("descripcion").innerHTML = prodMostrar.description;
-    document.getElementById("imgprod").src = prodMostrar.image;
+    document.getElementById("imgprod").src = prodImagen;
+    document.getElementById("cat").innerHTML=prodMostrar.Categoria;
     anadirNumBusqueda();
 }
-function buscarMas() {
-    prodBusqueda = JSON.parse(localStorage.getItem("selectedItem"));
-    allProducts=JSON.parse(localStorage.getItem("products"));
-    var name = prodMostrar.name;
-    if (!name)
-        filteredProducts = allProducts;
-    else {
-        filteredProducts = [];
-        for(var i=0; i<allProducts.length; i++) {
-            if (allProducts[i].name.toLowerCase().includes(name.toLowerCase())){
-                allProducts.splice(i,1);
-                allProducts[i].busquedas=nBusq;
-                console.log(allProducts[i].name+" "+allProducts[i].busquedas)
-            }
-        }
+function anadirNumBusqueda(){
+      prodMostrar = JSON.parse(localStorage.getItem("selectedItem"));
+        nBusq = parseInt(prodMostrar.busquedas)+1;
+        console.log(nBusq);
+        buscarMas();
     }
-}
+    function buscarMas() {
+        prodBusqueda = JSON.parse(localStorage.getItem("selectedItem"));
+            allProducts=JSON.parse(localStorage.getItem("products"));
+            var name = prodBusqueda.name;
+            if (!name)
+                   filteredProducts = allProducts;
+           else {
+                   filteredProducts = [];
+                   for(var i=0; i<allProducts.length; i++) {
+                            if (allProducts[i].name.toLowerCase().includes(name.toLowerCase())){
+                                   allProducts.splice(i,1);
+                                    allProducts[i].busquedas=nBusq;
+                                    console.log(allProducts[i].name+" "+allProducts[i].busquedas)
+                                }
+                        }
+               }
+        }
--- PAgina web 1/js/menu.js
+++ PAgina web 1/js/menu.js
@@ -7,18 +7,45 @@ btnMenu.addEventListener('click',function(){
 var allProducts;
 var filteredProducts;
 var page = 1;
+var allimagenes;
+var filteredimagenes;
+
 
 cargador();
 function cargador() {
     allProducts = JSON.parse(localStorage.getItem("products"));
+    allimagenes=JSON.parse(localStorage.getItem("imagenes"))
     if (!allProducts) {
         allProducts = [
-            {name: "Batman eau de toilette",busquedas:"0" ,categoria:"Hombre" ,description: "Batman es una fragancia oriental picante para hombres. La fragancia presenta almizcle, jengibre, pimienta negra, agarwood (oud), mirra y sándalo", precio: "50", cantidad: "5", image: "IMG/p6.jpg"},
-            {name: "Carolina Herrera - GOOD GIRL (Eau de perfume)",busquedas:"0" ,categoria:"Mujer" , description: "GOOD GIRL, es una fragancia para mujer oriental floral, que fue lanzada en 2016. El frasco es original y revolucionario en forma de zapato de tacón de aguja negro, con el tacón dorado, envuelto en cristal azul. La perfumista es Louise Turner. En la parte alta de la pirámide encontramos almendra y café, como notas medias cuenta con jazmín sambac y nardos y en la base permanecen haba tonka y cacao. Es una eau de perfume.", precio: "79.95", cantidad: "10", image: "IMG/p1.jpg"}
-                ];
+            {
+                name: "Batman eau de toilette",
+                Categoria: "Hombre",
+                description: "Batman es una fragancia oriental picante para hombres. La fragancia presenta almizcle, jengibre, pimienta negra, agarwood (oud), mirra y sándalo",
+                precio: "50",
+                busquedas: "0",
+                cantidad: "5"
+            },
+            {
+                name: "Carolina Herrera - GOOD GIRL (Eau de perfume)",
+                Categoria: "Mujer",
+                description: "GOOD GIRL, es una fragancia para mujer oriental floral, que fue lanzada en 2016. El frasco es original y revolucionario en forma de zapato de tacón de aguja negro, con el tacón dorado, envuelto en cristal azul. La perfumista es Louise Turner. En la parte alta de la pirámide encontramos almendra y café, como notas medias cuenta con jazmín sambac y nardos y en la base permanecen haba tonka y cacao. Es una eau de perfume.",
+                precio: "79.95",
+                busquedas: "0",
+                cantidad: "10"
+            }
+
+        ];
         localStorage.setItem("products", JSON.stringify(allProducts));
     }
+    if (!allimagenes) {
+        allimagenes= ["img/p6.jpg","img/p1.jpg"];
+        localStorage.setItem("imagenes",JSON.stringify(allimagenes));
+    }
+
+
+
     filteredProducts = allProducts;
+    filteredimagenes= allimagenes;
     mostrar();
 }
 
@@ -31,7 +58,7 @@ function mostrar() {
             document.getElementById("prod" + i).style.display = "none";
         } else {
             document.getElementById("prod" + i).style.display = "block";
-            document.getElementById("prod" + i + "-img").src = filteredProducts[arrayPos].image;
+            document.getElementById("prod" + i + "-img").src = filteredimagenes[arrayPos];
             document.getElementById("prod" + i + "-title").innerHTML = filteredProducts[arrayPos].name;
             document.getElementById("prod" + i + "-description").innerHTML = filteredProducts[arrayPos].description;
         }
@@ -39,33 +66,21 @@ function mostrar() {
 }
 function buscar() {
     var name = document.getElementById("buscador").value;
-    if (!name)
+    if (!name) {
         filteredProducts = allProducts;
+        filteredimagenes = allimagenes;
+    }
     else {
+        filteredimagenes= [];
         filteredProducts = [];
         for(var i=0; i<allProducts.length; i++) {
             if (allProducts[i].name.toLowerCase().includes(name.toLowerCase())){
                 filteredProducts.push(allProducts[i]);
+                filteredimagenes.push(allimagenes[i]);
             }
         }
-        mostrar();
     }
-}
-function buscarcategoria() {
-    var cate = document.getElementById("categoria").value;
-    console.log(cate);
-    if (!cate)
-        filteredProducts = allProducts;
-    else {
-        filteredProducts = [];
-        for(var i=0; i<allProducts.length; i++) {
-            if (allProducts[i].categoria.toLowerCase().includes(cate.toLowerCase())){
-                console.log(allProducts[i].name);
-                filteredProducts.push(allProducts[i]);
-            }
-        }
-         mostrar();
-    }
+    mostrar();
 }
 function prevPage() {
         if (page > 1) {
@@ -87,5 +102,24 @@ function nextPage() {
 function openDetails(viewPos) {
     arrayPos = viewPos - 1 + (page * 4 - 4);;
     localStorage.setItem("selectedItem", JSON.stringify(filteredProducts[arrayPos]));
+    localStorage.setItem("selectedimagen",JSON.stringify(filteredimagenes[arrayPos]));
 
+}
+function buscarcategoria() {
+    var Cate = document.getElementById("categoria").value;
+    if (!Cate) {
+        filteredProducts = allProducts;
+        filteredimagenes = allimagenes;
+    }
+    else {
+        filteredProducts = [];
+        filteredimagenes = [];
+        for(var i=0; i<allProducts.length; i++) {
+            if (allProducts[i].Categoria.toLowerCase().includes(Cate.toLowerCase())){
+                filteredProducts.push(allProducts[i]);
+                filteredimagenes.push(allimagenes[i]);
+            }
+        }
+    }
+    mostrar();
 }
