632360ab181cb1098e40405e357d56a50368d12d
aljiar <aljiar23@gmail.com>
2017-05-12 23:39:05 -0400

    Add files via upload

--- PAgina web 1/js/menu.js
+++ PAgina web 1/js/menu.js
@@ -13,8 +13,8 @@ function cargador() {
     allProducts = JSON.parse(localStorage.getItem("products"));
     if (!allProducts) {
         allProducts = [
-            {name: "Batman eau de toilette", description: "Batman es una fragancia oriental picante para hombres. La fragancia presenta almizcle, jengibre, pimienta negra, agarwood (oud), mirra y sándalo", precio: "50", cantidad: "5", image: "IMG/p6.jpg"},
-            {name: "Carolina Herrera - GOOD GIRL (Eau de perfume)", description: "GOOD GIRL, es una fragancia para mujer oriental floral, que fue lanzada en 2016. El frasco es original y revolucionario en forma de zapato de tacón de aguja negro, con el tacón dorado, envuelto en cristal azul. La perfumista es Louise Turner. En la parte alta de la pirámide encontramos almendra y café, como notas medias cuenta con jazmín sambac y nardos y en la base permanecen haba tonka y cacao. Es una eau de perfume.", precio: "79.95", cantidad: "10", image: "IMG/p1.jpg"}
+            {name: "Batman eau de toilette",busquedas:"0" ,categoria:"Hombre" ,description: "Batman es una fragancia oriental picante para hombres. La fragancia presenta almizcle, jengibre, pimienta negra, agarwood (oud), mirra y sándalo", precio: "50", cantidad: "5", image: "IMG/p6.jpg"},
+            {name: "Carolina Herrera - GOOD GIRL (Eau de perfume)",busquedas:"0" ,categoria:"Mujer" , description: "GOOD GIRL, es una fragancia para mujer oriental floral, que fue lanzada en 2016. El frasco es original y revolucionario en forma de zapato de tacón de aguja negro, con el tacón dorado, envuelto en cristal azul. La perfumista es Louise Turner. En la parte alta de la pirámide encontramos almendra y café, como notas medias cuenta con jazmín sambac y nardos y en la base permanecen haba tonka y cacao. Es una eau de perfume.", precio: "79.95", cantidad: "10", image: "IMG/p1.jpg"}
                 ];
         localStorage.setItem("products", JSON.stringify(allProducts));
     }
@@ -48,8 +48,24 @@ function buscar() {
                 filteredProducts.push(allProducts[i]);
             }
         }
+        mostrar();
     }
-    mostrar();
+}
+function buscarcategoria() {
+    var cate = document.getElementById("categoria").value;
+    console.log(cate);
+    if (!cate)
+        filteredProducts = allProducts;
+    else {
+        filteredProducts = [];
+        for(var i=0; i<allProducts.length; i++) {
+            if (allProducts[i].categoria.toLowerCase().includes(cate.toLowerCase())){
+                console.log(allProducts[i].name);
+                filteredProducts.push(allProducts[i]);
+            }
+        }
+         mostrar();
+    }
 }
 function prevPage() {
         if (page > 1) {
